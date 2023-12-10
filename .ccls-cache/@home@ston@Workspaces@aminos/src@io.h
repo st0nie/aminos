aca578b7f752a9ea74bd35aa736df2b9227a199b
ston <ston.jia@qq.com>
2023-12-10 15:07:11 +0800

    drop ccls-cache

--- .ccls-cache/@home@ston@Workspaces@aminos/src@io.h
+++
@@ -1,7 +0,0 @@
-#ifndef IO_H
-#define IO_H
-#include "public.h"
-
-int read_csv(long double[][ARRANGE_SIZE]);
-
-#endif
--- .ccls-cache/@home@ston@Workspaces@aminos/src@main.cpp
+++
@@ -1,7 +0,0 @@
-#include "io.h"
-#include "public.h"
-
-int main(){
-  long double arr[LINE_SIZE][ARRANGE_SIZE]; 
-  read_csv(arr);
-}
