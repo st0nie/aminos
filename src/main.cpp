0b825d521492495ba0dd9269a4541b15c3673fa2
ston <ston.jia@qq.com>
2023-12-17 18:56:13 +0800

    clang-format

--- src/main.cpp
+++ src/main.cpp
@@ -1,27 +1,34 @@
-#include <stdio.h>
+#include "io.h"
 #include "neural.h"
 #include "public.h"
-#include "io.h"
+#include <stdio.h>
 
 int main() {
   int layer_number = 2 + hidden_layer_number;
   neural_layer layers[layer_number];
   init_layer(layers, inputs);
   int i;
-  for (i=1;i<1+hidden_layer_number;i++){
-    init_layer(layers+i, hidden_layer_unit_number);
+  for (i = 1; i < 1 + hidden_layer_number; i++) {
+    init_layer(layers + i, hidden_layer_unit_number);
   }
-  init_layer(layers+i, 1);
-  for (i=0;i<layer_number;i++){
-    traverse_layer(layers+i);
-  }
-  double datas[3000][1+inputs];
+  init_layer(layers + i, 1);
+  // for (i = 0; i < layer_number; i++) {
+  //   traverse_layer(layers + i);
+  // }
+  double datas[3000][1 + inputs];
   get_inputs(datas);
-  for (i=0;i<2300;i++){
-    int j;
-    for (j=0;j<1+inputs;j++){
-      printf("%lf ",datas[i][j]);
-    }
-    putchar('\n');
+  // for (i = 0; i < 2300; i++) {
+  //   int j;
+  //   for (j = 0; j < 1 + inputs; j++) {
+  //     printf("%lf ", datas[i][j]);
+  //   }
+  //   putchar('\n');
+  // }
+  process_input(layers, datas[0]);
+  for (i = 0; i < layer_number - 1; i++) {
+    forward_propagation(layers + i, layers + i + 1);
+  }
+  for (i = 0; i < layer_number; i++) {
+    traverse_layer(layers + i);
   }
 }
--- src/neural.cpp
+++ src/neural.cpp
@@ -1,50 +1,57 @@
 #include "neural.h"
+#include <chrono>
 #include <math.h>
 #include <random>
-#include <chrono>
 #include <stdio.h>
 
-double sigmoid(double num) {
-  return 1/(1+exp(-num));
+double sigmoid(double num) { return 1 / (1 + exp(-num)); }
+
+neural_layer *forward_propagation(neural_layer *layer,
+                                  neural_layer *layer_next) {
+  int i;
+  for (i = 0; i < layer_next->number; i++) {
+    forward_propagation_unit(layer, &layer_next->units[i]);
+  }
+  return layer_next;
 }
 
-double forward_propagation(neural_layer* layer, neural_unit* unit) {
+double forward_propagation_unit(neural_layer *layer, neural_unit *unit) {
   int i;
-  int sum=0;
-  for (i=1;i<layer->number;i++){
-    sum+=layer->k[i][unit->index];
+  double sum = 0;
+  for (i = 0; i < layer->number; i++) {
+    sum += layer->k[i][unit->index] * layer->units[i].output;
   }
   sum += unit->bias;
   return (unit->output = sigmoid(sum));
 }
 
-neural_layer* process_input(neural_layer* input_layer,double array[]) {
+neural_layer *process_input(neural_layer *input_layer, double array[]) {
   int i;
-  for (i=0;i<input_layer->number;i++){
+  for (i = 0; i < input_layer->number; i++) {
     input_layer->units[i].output = array[i];
   }
   return input_layer;
 }
 
-neural_layer *init_layer(neural_layer *layer_p,int number){
-  layer_p->units = (neural_unit*)malloc(sizeof(neural_unit[number]));
+neural_layer *init_layer(neural_layer *layer_p, int number) {
+  layer_p->units = (neural_unit *)malloc(sizeof(neural_unit[number]));
   neural_unit *p = layer_p->units;
   int i;
-  for(i=0;i<number;i++){
+  for (i = 0; i < number; i++) {
     init_unit(p, i);
     p++;
   }
   layer_p->number = number;
   int j;
-  for(j=0;j<50;j++){
-    for(i=0;i<50;i++){
+  for (j = 0; j < 50; j++) {
+    for (i = 0; i < 50; i++) {
       layer_p->k[i][j] = rand_gause();
     }
   }
   return layer_p;
 }
 
-neural_unit *init_unit(neural_unit *unit_p,int index){
+neural_unit *init_unit(neural_unit *unit_p, int index) {
   unit_p->bias = rand_gause();
   unit_p->index = index;
   unit_p->output = 0;
@@ -54,21 +61,19 @@ neural_unit *init_unit(neural_unit *unit_p,int index){
 double rand_gause() {
   unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
   std::default_random_engine gen(seed);
-  std::normal_distribution<double> distribution(0,1);
-  double result;
-  while((result = distribution(gen))<=0 || result >= 1);
-  return result;
+  std::normal_distribution<double> distribution(0, 1);
+  return distribution(gen);
 }
 
 neural_layer *traverse_layer(neural_layer *layer) {
   if (layer) {
     int i;
-    printf("unit_numbers: %d\n",layer->number);
+    printf("unit_numbers: %d\n", layer->number);
     putchar('\n');
     for (i = 0; i < layer->number; i++) {
-      printf("unit: %d\n",layer->units[i].index);
-      printf("bias: %lf\n",layer->units[i].bias);
-      printf("output: %lf\n",layer->units[i].output);
+      printf("unit: %d\n", layer->units[i].index);
+      printf("bias: %lf\n", layer->units[i].bias);
+      printf("output: %lf\n", layer->units[i].output);
       putchar('\n');
     }
   } else {
